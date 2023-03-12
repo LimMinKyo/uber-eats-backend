4f6e30b82dc7290e4186dd62b72c32c596a67f5f
LimMinKyo <story1400@naver.com>
2023-03-12 18:01:06 +0900

    ADD: TypeORM Entitiy on retaurant.entity.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import * as Joi from 'joi';
+import { Restaurant } from './restaurants/entities/restaurant.entity';
 
 @Module({
   imports: [
@@ -28,8 +29,9 @@ import * as Joi from 'joi';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      synchronize: true,
+      synchronize: process.env.NODE_ENV !== 'prod',
       logging: true,
+      entities: [Restaurant],
     }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,16 +1,30 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 @ObjectType()
 export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field(() => Number)
+  id: number;
+
+  @Column()
   @Field(() => String)
   name: string;
 
+  @Column()
   @Field(() => Boolean)
   isVegan: boolean;
 
+  @Column()
   @Field(() => String)
   address: string;
 
+  @Column()
   @Field(() => String)
   ownersName: string;
+
+  @Column()
+  @Field(() => String)
+  categoryName: string;
 }
