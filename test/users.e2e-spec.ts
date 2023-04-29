d39fcd4f5bb7474e56d4eeb87b17ffded7651c99
LimMinKyo <story1400@naver.com>
2023-04-30 08:13:21 +0900

    ADD: createAccount e2e test

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,6 +2,15 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
 import { DataSource } from 'typeorm';
+import * as request from 'supertest';
+
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UsersModule (e2e)', () => {
   let app: INestApplication;
@@ -30,7 +39,58 @@ describe('UsersModule (e2e)', () => {
     await app.close();
   });
 
-  it.todo('createAccount');
+  describe('createAccount', () => {
+    const EMAIL = 'test@test.com';
+
+    it('should create account.', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+            mutation {
+              createAccount(input: {
+                email: "${EMAIL}",
+                password: "12345",
+                role: Owner
+              }) {
+                ok,
+                error
+              }
+            }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it('should fail if account already exists.', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+            mutation {
+              createAccount(input: {
+                email: "${EMAIL}",
+                password: "12345",
+                role: Owner
+              }) {
+                ok,
+                error
+              }
+            }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toEqual(
+            'There is a user with that email already',
+          );
+        });
+    });
+  });
+
   it.todo('login');
   it.todo('me');
   it.todo('userProfile');
