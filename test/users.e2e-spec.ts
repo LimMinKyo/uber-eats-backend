47dbe2293b7dd7ab3b889ecb42a1050b2897fa6d
LimMinKyo <story1400@naver.com>
2023-04-30 07:31:00 +0900

    ADD: e2e Test Setup

---
+++ test/users.e2e-spec.ts
@@ -0,0 +1,38 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+import { DataSource } from 'typeorm';
+
+describe('UsersModule (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    const dataSource = new DataSource({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
+    const connection = await dataSource.initialize();
+    await connection.dropDatabase();
+    await connection.destroy();
+    await app.close();
+  });
+
+  it.todo('createAccount');
+  it.todo('login');
+  it.todo('me');
+  it.todo('userProfile');
+  it.todo('verifyEmail');
+});
