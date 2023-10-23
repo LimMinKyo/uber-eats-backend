14161942210fe6d71a0b3f6240d80cbff2000e0f
LimMinKyo <story1400@naver.com>
2023-10-23 21:04:51 +0900

    UPT: envFilePath

--- src/app.module.ts
+++ src/app.module.ts
@@ -29,7 +29,12 @@ const TOKEN_KEY = 'x-jwt';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      envFilePath:
+        process.env.NODE_ENV === 'dev'
+          ? '.env.dev'
+          : process.env.NODE_ENV === 'prod'
+          ? '.env.prod'
+          : '.env.test',
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
