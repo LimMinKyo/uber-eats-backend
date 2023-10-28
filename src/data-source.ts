93649b4e5ae1315c92c4dfe0b4203b517089d9f6
LimMinKyo <story1400@naver.com>
2023-10-28 17:33:51 +0900

    ADD: seed setting

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,22 +1,34 @@
 import { ConfigService } from '@nestjs/config';
 import { config } from 'dotenv';
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { SeederOptions } from 'typeorm-extension';
 
-config();
+config({
+  path:
+    process.env.NODE_ENV === 'development'
+      ? '.env.development'
+      : process.env.NODE_ENV === 'production'
+      ? '.env'
+      : '.env.test',
+});
 
 const configService = new ConfigService();
 
-export default new DataSource({
+const options: DataSourceOptions & SeederOptions = {
   type: 'postgres',
   host: configService.get('DB_HOST'),
   port: configService.get<number>('DB_PORT'),
   username: configService.get('DB_USERNAME'),
   password: configService.get('DB_PASSWORD'),
   database: configService.get('DB_NAME'),
-  synchronize: false,
+  synchronize: true,
   logging:
     process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test',
   entities: ['src/**/*.entity.ts'],
   migrations: ['src/database/migrations/*.ts'],
   migrationsTableName: 'migrations',
-});
+  seeds: ['src/database/seeds/**/*{.ts,.js}'],
+  factories: ['src/database/factories/**/*{.ts,.js}'],
+};
+
+export default new DataSource(options);
